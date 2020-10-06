d103e692bb2fd0e0738ff5bb0fe3009d936c0dad
Roberta Moreira <30378788+Robi9@users.noreply.github.com>
2020-10-05 21:49:59 -0300

    Update conexaoBD.js

--- back-end/conexaoBD.js
+++ back-end/conexaoBD.js
@@ -10,3 +10,21 @@ mongoose.connect('mongodb://localhost:27017/teste').then((result) => { //lembra 
 }).catch((err) => {
 	console.log('Deu erro..' + err)
 });
+
+//LEMBRA DE MUDAR PRA O SEU BANCO
+var Schema = mongoose.Schema;
+var userDataSchema = new Schema({  
+ nome: {type: String, required: true},  
+ email: String,  
+ telefone: String  
+}, {collection: 'cadastro'});
+
+var Cadastro = mongoose.model('UserData', userDataSchema); 
+
+var pessoa = {  
+   nome: "Roberta",  
+   email: "r@gail.com",  
+   telefone: "88992087582"  
+ };  
+ var data = new Cadastro(pessoa);  
+ data.save();
