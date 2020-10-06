31ad8128d36ea46f0adda95252e8a4322e099912
mfelixss <mateusffelix.ss@gmail.com>
2020-10-05 22:11:59 -0300

    update routes

---
+++ back-end/routes.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const { request } = require('http')
+const bodyparser = require('body-parser')
+const portaHttp = 9999
+
+// Chamada dos Controllers
+const homeController = require('./controllers/homeController')
+const categoriaController = require('./controllers/categoriaController')
+const lojaController = require('./controllers/lojaController')
+const detalhesController = require('./controllers/detalhesController')
+const carrinhoControlller = require('./controllers/carrinhoController')
+const pagamentoController = require('./controllers/pagamentoController')
+const cadastroClienteController = require('./controllers/cadastroCienteController')
+const logiController = require('./controllers/loginController')
+const admEmpresaController = require('./controllers/admEmpresaController')
+const admSiteController = require('./controllers/admSiteController')
+
+// Inicializaçao do express
+const routes = express()
+
+// Rotas Express
+
+routes.get('/home', (request, response) => {
+    response.send('Home ta funcionando')
+});
+
+routes.get('/categoria', (request, response) => {
+    response.send('Categoria ta funcionando')
+});
+
+routes.get('/loja', (request, response) => {
+    response.send('Loja ta funcionando')
+});
+
+routes.get('/detalhes', (request, response)  => {
+    response.send('Detalhes tá funcionando')
+});
+
+routes.get('/carrinho', (request, response) => {
+    response.send('Carrinho tá funcionando')
+});
+
+routes.get('/pagamento', (request, response) => {
+    response.send('Pagamento ta funcionando')
+});
+
+routes.get('/cadastroCliente', (request, response) => {
+    response.send('cadastroCliente ta funcionando')
+});
+
+routes.get('/login', (request, response) => {
+    response.send('Login ta funcionando')
+});
+
+routes.get('/admEmpresa', (request, response) => {
+    response.send('admEmpresa ta funcionando')
+});
+
+routes.get('/admSite', (request, response) => {
+    response.send('admSite ta funcionando')
+});
+
+
+module.exports = routes;
+
+routes.listen(portaHttp, () => {
+    console.log('Servidor executando na porta: ' + portaHttp)
+});
